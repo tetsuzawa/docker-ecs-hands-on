53a55ed71cab73f2fb180f2b349672babbcd6196
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-05-04 19:54:00 +0900

    fix

--- example/infrastructure/route.tf
+++ example/infrastructure/route.tf
@@ -3,3 +3,15 @@ resource "aws_route" "hands_on_public_1a_igw" {
   gateway_id = aws_internet_gateway.hands_on.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+resource "aws_route" "hands_on_public_1c_igw" {
+  route_table_id = aws_route_table.hands_on_public_1c.id
+  gateway_id = aws_internet_gateway.hands_on.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "hands_on_public_1d_igw" {
+  route_table_id = aws_route_table.hands_on_public_1d.id
+  gateway_id = aws_internet_gateway.hands_on.id
+  destination_cidr_block = "0.0.0.0/0"
+}
