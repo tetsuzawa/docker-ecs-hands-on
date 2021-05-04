c96f028a6666d61973e743fe68347b0cf1dd6768
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-05-04 19:22:45 +0900

    fix

--- example/infrastructure/s3.tf
+++ example/infrastructure/s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "hands_on_credentials" {
-  bucket = "hands-on-credentials"
-  acl    = "private"
-
-  tags = {
-    Name        = "hands-on-credentials"
-  }
-}
+//resource "aws_s3_bucket" "hands_on_credentials" {
+//  bucket = "hands-on-credentials"
+//  acl    = "private"
+//
+//  tags = {
+//    Name        = "hands-on-credentials"
+//  }
+//}
--- example/infrastructure/securty_group.tf
+++ example/infrastructure/securty_group.tf
@@ -22,3 +22,78 @@ resource "aws_security_group" "hands_on_alb" {
     Name = "hands-on-alb"
   }
 }
+
+# for web
+resource "aws_security_group" "hands_on_web" {
+  name        = "hands-on-web"
+  vpc_id      = aws_vpc.hands_on.id
+
+  ingress {
+    description      = "HTTP VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.hands_on.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "hands-on-web"
+  }
+}
+
+# for service1
+resource "aws_security_group" "service1" {
+  name        = "service1"
+  vpc_id      = aws_vpc.hands_on.id
+
+  ingress {
+    description      = "HTTP VPC"
+    from_port        = 9001
+    to_port          = 9001
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.hands_on.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "hands-on-service1"
+  }
+}
+
+# for service2
+resource "aws_security_group" "service2" {
+  name        = "service2"
+  vpc_id      = aws_vpc.hands_on.id
+
+    ingress {
+      description      = "HTTP VPC"
+      from_port        = 9002
+      to_port          = 9002
+      protocol         = "tcp"
+      cidr_blocks      = [aws_vpc.hands_on.cidr_block]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "hands-on-service2"
+  }
+}
